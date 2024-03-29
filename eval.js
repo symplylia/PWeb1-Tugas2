8c3cfdc1c31f0c85e5d989e1a8aef01f50aa044e
symplylia <165117823+symplylia@users.noreply.github.com>
2024-03-28 15:33:18 -1200

    Add files via upload

---
+++ eval.js
@@ -0,0 +1,12 @@
+function hitung(a,b){
+    return a*b;
+  }
+  var a = 20;
+  var b = 9;
+  var hasil= hitung(5,9);
+  document.write("Bilangan dari Tabel A Baris ke 3 = " + a + "<br>");
+  document.write("Bilangan dari Tabel B Baris ke 4 = " + b + "<br>");
+  
+  function button() {
+    alert("Bilangan A * B = " + hasil);
+  }
